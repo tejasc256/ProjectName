f0acbaf071af9dcb6ea0e2a757fa748d8aba8352
sjs253 <shreyansh25.shrivastava@gmail.com>
2019-02-05 21:41:19 +0530

    Dashboard files added

---
+++ app/src/main/java/com/example/android/projectname/PageViewerAdapter.java
@@ -0,0 +1,33 @@
+package com.example.android.projectname;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+class PagerViewAdapter extends FragmentPagerAdapter {
+    public PagerViewAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        Fragment fragment = null;
+        switch (position){
+
+            case 0:
+                fragment = new fragment_page1();
+                break;
+            case 1:
+                fragment = new fragment_page2();
+                break;
+        }
+
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+}
